c9717834ebe0ce82d6d75268aa062c789d295709
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2024-01-28 14:16:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ const app = express();
 const server = createServer(app);
 
 //middleware
-app.use(
-	cors({
-		origin: "https://blogify-hub-frontend.vercel.app/",
-		credentials: true,
-		methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-	})
-);
+const corsOptions = {
+	origin: "http://localhost:5173",
+	credentials: true,
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
